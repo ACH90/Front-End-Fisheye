1d1852f5c566b1630426a2a6ea2ddd4f5d98c41c
ACH90 <chassagne.alexandre@gmail.com>
2024-09-10 15:47:12 +0200

    Liens cliquables sur zone foccussable contant img et h2 des photographes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -6,6 +6,11 @@ function photographerTemplate(data) {
   function getUserCardDOM() {
     const article = document.createElement("article");
 
+    // Création d'un lien focussable qui englobera l'image et le titre
+    const link = document.createElement("a");
+    link.setAttribute("href", "#"); // L'URL peut être ajustée pour chaque photographe
+    link.classList.add("focusable-link");
+
     // Création de l'image du photographe
     const img = document.createElement("img");
     img.setAttribute("src", picture);
@@ -14,6 +19,11 @@ function photographerTemplate(data) {
     // Création du nom du photographe
     const h2 = document.createElement("h2");
     h2.textContent = name;
+
+    // Ajout de l'image et du titre dans le lien
+    link.appendChild(img);
+    link.appendChild(h2);
+    link.setAttribute("alt", "");
 
     // Création de l'élément contenant la localisation
     const location = document.createElement("p");
@@ -31,8 +41,7 @@ function photographerTemplate(data) {
     priceElement.classList.add("photographer-price");
 
     // Ajout des éléments au "article"
-    article.appendChild(img);
-    article.appendChild(h2);
+    article.appendChild(link); // Ajout du lien contenant l'image et le titre
     article.appendChild(location);
     article.appendChild(taglineElement);
     article.appendChild(priceElement);
