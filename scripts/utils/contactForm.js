1c875bb2353184191611af64abbdb220aa1bdbb9
ACH90 <chassagne.alexandre@gmail.com>
2024-10-17 14:13:18 +0200

    Deplacer code js modale vers contactForm.js et modif code

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,37 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+}
+
+//Recupération des données du formulaire de contact (Modale)
+
+// Ajouter un écouteur d'événements pour l'envoi du formulaire
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    // Empêche la soumission du formulaire et le rechargement de la page
+    event.preventDefault();
+
+    // Appel de la fonction pour obtenir les valeurs du formulaire
+    const formValues = getFormValues();
+
+    // Vous pouvez traiter ces données ou les envoyer à un serveur ici
+    console.log("Formulaire soumis avec succès :", formValues);
+  });
+
+// Fonction pour récupérer les valeurs du formulaire
+function getFormValues() {
+  const firstName = document.getElementById("first_name").value;
+  const lastName = document.getElementById("last_name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  closeModal();
+
+  return { firstName, lastName, email, message };
 }
