bb45ed9f9ebed7c0578f4d174032c3fd886a8f0f
ACH90 <chassagne.alexandre@gmail.com>
2024-10-18 10:56:58 +0200

    Add getFormValues

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -193,3 +193,29 @@ Promise.all([
   displayPhotographerData(photographer); // Affiche les infos du photographe
   displayPhotographerMedia(media); // Affiche les œuvres du photographe
 });
+
+//Recupération des données du formulaire de contact (Modale)
+
+// Ajouter un écouteur d'événements pour l'envoi du formulaire
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    // Empêche la soumission du formulaire et le rechargement de la page
+    event.preventDefault();
+
+    // Appel de la fonction pour obtenir les valeurs du formulaire
+    const formValues = getFormValues();
+
+    // Vous pouvez traiter ces données ou les envoyer à un serveur ici
+    console.log("Formulaire soumis avec succès :", formValues);
+  });
+
+// Fonction pour récupérer les valeurs du formulaire
+function getFormValues() {
+  const firstName = document.getElementById("first_name").value;
+  const lastName = document.getElementById("last_name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  return { firstName, lastName, email, message };
+}
