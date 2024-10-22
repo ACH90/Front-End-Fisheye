c4f24c470774df4615cae88b838e04272ec73883
ACH90 <chassagne.alexandre@gmail.com>
2024-10-22 15:54:20 +0200

    Add dropdown html css et js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -338,3 +338,42 @@ function closeModalCarousel() {
 
   currentSlideIndex = 0;
 }
+
+//-------------------------------------------------------------
+
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdownItems = document.querySelectorAll(".dropdown-item");
+
+// Fonction pour ouvrir/fermer le dropdown
+dropdownToggle.addEventListener("click", () => {
+  const expanded =
+    dropdownToggle.getAttribute("aria-expanded") === "true" || false;
+  dropdownToggle.setAttribute("aria-expanded", !expanded);
+});
+
+// Fonction pour gérer la sélection d'un élément
+dropdownItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    // Mettre à jour le texte du bouton avec l'option sélectionnée
+    dropdownToggle.innerHTML =
+      item.textContent + ' <span class="arrow">&#9660;</span>';
+
+    // Fermer le menu
+    dropdownToggle.setAttribute("aria-expanded", false);
+
+    // Ajouter une classe active à l'élément sélectionné
+    dropdownItems.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+
+// Fermer le dropdown si on clique en dehors
+document.addEventListener("click", function (event) {
+  if (
+    !dropdownToggle.contains(event.target) &&
+    !dropdownMenu.contains(event.target)
+  ) {
+    dropdownToggle.setAttribute("aria-expanded", false);
+  }
+});
